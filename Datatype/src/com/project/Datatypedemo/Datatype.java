f582dac3d185c086634c300dd768d4795b7618ff
LENOVO <LENOVO@DESKTOP-TGDAT0J>
2023-02-27 11:34:54 +0530

    Datatype

---
+++ Datatype/src/com/project/Datatypedemo/Datatype.java
@@ -0,0 +1,34 @@
+package com.project.Datatypedemo;
+
+public class Datatype {
+public static void main(String[] args) {
+	//---------Primitive Datatypes------->
+	byte bytevalue=125;
+	System.out.println("the byte value is: "+bytevalue);
+	short shortvalue=32654;
+	System.out.println("the short value is :"+shortvalue);
+	char character='G';
+	System.out.println("the Character value is :"+character);
+	int integer=10000;
+	System.out.println("the integer value is :"+integer);
+	float floatvalue=150.235f;
+	System.out.println("the float value is :"+floatvalue);
+	double doublevalue=12356.44434343355d;
+	System.out.println("the double value is :"+doublevalue);
+	long contactnumber=332243244443544l;
+	System.out.println("the contact number is :"+contactnumber);
+	boolean issaved=true;
+	System.out.println("the boolean value is :"+issaved);
+	//--------Non-Primitive Datatype--------->
+	String name="Sharath";
+	System.out.println("the value of name is :"+name);
+	String symbols="@#@$%#@%$%$%$%$";
+	System.out.println("the value of symbols is :"+symbols);
+	String fractionvalue="323244.244434";
+	System.out.println("the fraction value is :"+fractionvalue);
+	String contactno="32432535435";
+	System.out.println("the contact no valuve is :"+contactno);
+	String values="true";
+	System.out.println("tha boolean value is :"+values);
+}
+}
